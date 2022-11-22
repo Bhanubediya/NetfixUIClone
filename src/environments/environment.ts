9ecbea454caa4df5445aa19e9c43d6cd6b422186
Bhanubediya <bhanubediya@gmail.com>
2022-11-22 12:46:23 +0530

    v2.3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   API_key:'07548f2713e807ba2133b6202f4eae39'
 };
 
